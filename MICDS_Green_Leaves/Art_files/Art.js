1336c4954785eb0a25543980a463f9441a97770b
Rohit Srivastava <rsrivastava@github.com>
2014-03-03 09:58:36 -0600

    update site to be more finished and add art page

--- MICDS_Green_Leaves/Art_files/Art.js
+++ MICDS_Green_Leaves/Art_files/Art.js
@@ -3,7 +3,7 @@
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
-{loadMozillaCSS('Poetry_files/PoetryMoz.css')
+{loadMozillaCSS('Art_files/ArtMoz.css')
 adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
--- MICDS_Green_Leaves/Prose_files/Prose.js
+++ MICDS_Green_Leaves/Prose_files/Prose.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20140302
+// Created by iWeb 3.0.4 local-build-20140303
 
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
